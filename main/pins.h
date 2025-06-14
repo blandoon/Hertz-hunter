7c981e8f6418894f02a31745cc2578a19d261192
Blandon Ray <blandoon@gmail.com>
2025-06-14 14:21:16 -0700

    Update pins.h

--- main/pins.h
+++ main/pins.h
@@ -1,18 +1,19 @@
 #ifndef PINS_H
 #define PINS_H
 
-#define PREVIOUS_BUTTON_PIN 21
-#define SELECT_BUTTON_PIN 20
-#define NEXT_BUTTON_PIN 10
+// Edited for Adafruit FeatherWing OLED with built in buttons
+#define PREVIOUS_BUTTON_PIN 9
+#define SELECT_BUTTON_PIN 6
+#define NEXT_BUTTON_PIN 5
 
-#define SPI_DATA_PIN 6
-#define SPI_LE_PIN 7
-#define SPI_CLK_PIN 4
+#define SPI_DATA_PIN 37
+#define SPI_LE_PIN 10
+#define SPI_CLK_PIN 36
 
-#define RSSI_PIN 3
+#define RSSI_PIN 15
 
-#define BUZZER_PIN 2
+#define BUZZER_PIN 18
 
-#define BATTERY_PIN 0
+#define BATTERY_PIN 17
 
 #endif
